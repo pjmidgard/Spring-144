a5184690cec0c66797beff040edd9b2bc8757309
Java Software <pj.midgard@yahoo.com>
2022-10-15 17:00:35 +0100

    Update Spring-144.py

--- Spring-144.py
+++ Spring-144.py
@@ -1,6 +1,7 @@
 #Spring-144 
 
 #Algorithm Spring-143 and paq9a
+#Algorithm Spring-143
 
 #We should take 64 bits and find two the same #variations for 32-64 bits long and 0-64 bit, then, #save where it is a 3 bits variation, 8 combination #and 4 bits variation, 16 combination. Add one bit #next and try to find again if not find that block it's #compress and find variation should be random.
 
