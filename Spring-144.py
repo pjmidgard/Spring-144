dd24e5408a98c9fc90c67becf051d611ba07af03
Java Software <pj.midgard@yahoo.com>
2022-10-16 09:41:40 +0100

    Update Spring-144.py

--- Spring-144.py
+++ Spring-144.py
@@ -1,10 +1,9 @@
 #Spring-144
 
-#Algorithm Spring-143 and paq9a:
 
-#Algorithm Spring-143
+#Algorithm Spring-144
 
-#We should take 64 bits and find two the same #variations for 32-64 bits long and 0-64 bit, then, #save where it is a 3 bits variation, 8 combination #and 4 bits variation, 16 combination. Add one bit #next and try to find again if not find that block it's #compress and find variation should be random.
+#We should take 64 bits and find two the same #variations for 32-64 bits long and 0-64 bit, then, #save where it is a 3 bits variation, 8 combination #and 4 bits variation, 16 combination. Add one bit #next and try to find again if not find that block it's #compress  find variation should be random.
 
 from time import time
 cvf=0
